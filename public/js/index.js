34c0a8a213d1b561fb03c953078f49bcb71580ea
Mayerson <mayerson18@gmail.com>
2017-10-04 18:27:17 -0400

    contador

--- public/js/index.js
+++ public/js/index.js
@@ -16,10 +16,29 @@ $('.carrusel2').owlCarousel({
   items: 3,
   margin: 50,
   autoplay:true,
-  autoplayTimeout:5000
+  autoplayTimeout:5000,
+  responsiveClass: true,
+  responsive:{
+       0:{
+           items:1
+       },
+       600:{
+           items:1
+       },
+       1000:{
+           items:3,
+           loop:true
+       }
+   },
 });
 
 $(".click").click(function(){
-  console.log("next")
-  $(".owl-next").click();
+  $(this).parent().parent().parent().parent().parent().find(".owl-nav").find(".owl-next").click();
 })
+
+var now = new Date();
+var fecha = new Date("2018-06-14 23:46:48 UTC");//La fecha destino del evento
+var	days = (fecha - now) / 1000 / 60 / 60 / 24;
+var daysRound = Math.floor(days);
+
+$(".red").text(daysRound);
