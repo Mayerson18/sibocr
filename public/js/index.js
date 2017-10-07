5dd00fbc01596d78247abcfb0f616bceb6f7873f
Mayerson <mayerson18@gmail.com>
2017-10-07 15:13:46 -0400

    submit

--- public/js/index.js
+++ public/js/index.js
@@ -179,5 +179,13 @@ $('.form').validate({ // initialize the plugin
        }
     });
 
+    $(".form").on('keyup keypress', function(e) {
+      var keyCode = e.keyCode || e.which;
+       if (keyCode === 13) {
+         e.preventDefault();
+         return false;
+       }
+    });
+
 
 });
